e8575d516daaac321020e735d4b82a3a5f435a3b
joelhi <d.j.hilmersson@gmail.com>
2024-01-20 14:23:12 +0000

    add login method, error handling and minor refactoring

--- src/layout.py
+++ src/layout.py
@@ -15,44 +15,61 @@ COLOR_ORANGE = colour(200, 100, 20)
 COLOR_BLACK = colour(0, 0, 0)
 
 COLOURS = {
-        4: COLOR_RED,
-        1: COLOR_GREEN,
-        2: COLOR_YELLOW,
-        3: COLOR_ORANGE,
-    }
+    4: COLOR_RED,
+    1: COLOR_GREEN,
+    2: COLOR_YELLOW,
+    3: COLOR_ORANGE,
+}
 
-def connect_to_wifi(lcd : LCD_1inch8):
+
+def connect_to_wifi(lcd: LCD_1inch8):
     lcd.fill(lcd.BLACK)
     lcd.text("connecting...", 5, 5, lcd.WHITE)
     lcd.show()
     connect()
-    lcd.fill(colour(20,180,75))
+    lcd.fill(colour(20, 180, 75))
     lcd.text("connected!", 5, 5, lcd.WHITE)
     lcd.show()
     time.sleep(1)
 
+
 def render_glucose_value(lcd: LCD_1inch8, value: float):
     integer_part, decimal_part = divmod(value, 1)
-    
+
     integer_digits = str(int(integer_part))
     decimal_digit = str(int(decimal_part * 10))
-    
-    integer = [integer_digits[0] if len(integer_digits) > 0 else " ",
-               integer_digits[1] if len(integer_digits) > 1 else " "]
-    
+
+    integer = [
+        integer_digits[1] if len(integer_digits) > 1 else " ",
+        integer_digits[0] if len(integer_digits) > 0 else " ",
+    ]
+
     draw_numbers(lcd, integer, SCALE, 15, 30)
-    draw_numbers(lcd, [".", decimal_digit], int(SCALE / 2), int(SCALE + 15 + 2 * 8 * 5), int(30 + (8 * 7 / 2)))
+    draw_numbers(
+        lcd,
+        [".", decimal_digit],
+        int(SCALE / 2),
+        int(SCALE + 15 + 2 * 8 * 5),
+        int(30 + (8 * 7 / 2)),
+    )
 
 
 def run_layout():
     lcd = setup_lcd()
     connect_to_wifi(lcd)
 
-    while(True):
-        glucose_info = fetch_glucose_data()
+    while True:
+        (success, glucose_info) = fetch_glucose_data()
+        if not success:
+            lcd.fill(COLOR_BLACK)
+            lcd.text("Failed to read glucose value, trying again...", 5, 5, lcd.WHITE)
+            lcd.show()
+            time.sleep(10)
+            continue
+
         lcd.fill(COLOURS.get(glucose_info[3], COLOR_BLACK))
         render_glucose_value(lcd, glucose_info[0])
-        lcd.text("Updated: "+glucose_info[1].split(" ")[1], 5, 5, lcd.WHITE)
+        lcd.text("Updated: " + glucose_info[1].split(" ")[1], 5, 5, lcd.WHITE)
         draw_trend_arrow(lcd, 5, 95, 3, glucose_info[2])
         lcd.show()
-        time.sleep(30)
+        time.sleep(45)
